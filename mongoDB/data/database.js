6c41369331af67a5dba5d575b455dad353486798
Christian Vivoli <christian280504@gmail.com>
2023-10-23 15:26:15 +0200

    aggiunta nuovi progetti

--- mongoDB/data/database.js
+++ mongoDB/data/database.js
@@ -5,7 +5,7 @@ const MongoClient = mongodb.MongoClient;
 let database;
 
 async function connect() {
-    const client = await MongoClient.connect('mongodb://root:root@127.0.0.1:27017');
+    const client = await MongoClient.connect('mongodb://root:root@127.0.0.1:27017');//mongodb://127.0.0.1:27017
     database = client.db('blog');
 }
 
